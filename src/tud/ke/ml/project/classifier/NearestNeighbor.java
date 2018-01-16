6226af2f19b6dd1641b7d116ec02bb7191169832
cbiehl <clemens.biehl@sap.com>
2018-01-16 13:06:22 +0100

    First edit

--- src/tud/ke/ml/project/classifier/NearestNeighbor.java
+++ src/tud/ke/ml/project/classifier/NearestNeighbor.java
@@ -1,8 +1,13 @@
 package tud.ke.ml.project.classifier;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import tud.ke.ml.project.util.Pair;
@@ -14,22 +19,28 @@ import tud.ke.ml.project.util.Pair;
 public class NearestNeighbor extends INearestNeighbor implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	private List<List<Object>> model;
+	
 	protected double[] scaling;
 	protected double[] translation;
 
 	@Override
 	public String getMatrikelNumbers() {
-		throw new NotImplementedException();
+		return "2629955,2493301,2792549";
 	}
 
 	@Override
 	protected void learnModel(List<List<Object>> data) {
-		throw new NotImplementedException();
+		this.model = data;
 	}
 
 	@Override
 	protected Map<Object, Double> getUnweightedVotes(List<Pair<List<Object>, Double>> subset) {
-		throw new NotImplementedException();
+//		for(Pair<List<Object>, Double> instance : subset) {
+//			
+//		}
+		
+		return null;
 	}
 
 	@Override
@@ -44,17 +55,50 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 
 	@Override
 	protected Object vote(List<Pair<List<Object>, Double>> subset) {
-		throw new NotImplementedException();
+		return getWinner(getUnweightedVotes(subset));
 	}
 
 	@Override
 	protected List<Pair<List<Object>, Double>> getNearest(List<Object> data) {
-		throw new NotImplementedException();
+		List<Pair<List<Object>, Double>> instances = new ArrayList<Pair<List<Object>, Double>>();
+		
+		for(List<Object> instance : this.model) {
+			Double d = determineManhattanDistance(instance, data);
+			Pair<List<Object>, Double> p = new Pair<List<Object>, Double>(instance, d);
+			instances.add(p);
+		}
+		
+		Collections.sort(instances, new Comparator<Pair<List<Object>, Double>>() {
+			@Override
+			public int compare(Pair<List<Object>, Double> o1, Pair<List<Object>, Double> o2) {
+				Double d1 = o1.getB();
+				Double d2 = o2.getB();
+				
+				return d1.compareTo(d2);
+			}
+		});
+		
+		List<Pair<List<Object>, Double>> nearest = instances.subList(0, getkNearest());
+		
+		return nearest;
 	}
 
 	@Override
 	protected double determineManhattanDistance(List<Object> instance1, List<Object> instance2) {
-		throw new NotImplementedException();
+		double distance = 0.0;
+		
+		for(int i=0; i < instance1.size(); i++) {
+			if(instance1.get(i) instanceof Double && instance2.get(i) instanceof Double) {
+				distance += Math.abs((Double) instance1.get(i) - (Double) instance2.get(i));	
+			}
+			else if(instance1.get(i) instanceof String && instance2.get(i) instanceof String) {
+				if(! instance1.get(i).equals(instance2.get(i))) {
+					distance += 1;
+				}
+			}
+		}
+		
+		return distance;
 	}
 
 	@Override
@@ -66,5 +110,18 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 	protected double[][] normalizationScaling() {
 		throw new NotImplementedException();
 	}
-
+	
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+	    return map.entrySet()
+	              .stream()
+	              .sorted(Map.Entry.comparingByValue())
+	              .collect(Collectors.toMap(
+		               Map.Entry::getKey, 
+		               Map.Entry::getValue, 
+		               (e1, e2) -> e1, 
+		               LinkedHashMap::new
+	              ));
+	}
+	
 }
+ 
