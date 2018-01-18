38e75a489ef51059cba7753452571a0fb8241ad3
DaWe1992 <daniel.wehner92@gmx.de>
2018-01-18 15:45:58 +0100

    bla2

--- src/tud/ke/ml/project/classifier/NearestNeighbor.java
+++ src/tud/ke/ml/project/classifier/NearestNeighbor.java
@@ -161,7 +161,7 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 	}
 
 	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
-		return map.entrySet().stream().sorted(Map.Entry.comparingByValue())
+		return map.entrySet().stream().sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
 				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 	}
 
